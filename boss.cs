8b24fce86d4815c559e789b2c53eccc39d71256a
Faktorfak <101659073+Faktorfak@users.noreply.github.com>
2022-12-04 19:59:59 +0200

    Update boss.cs

--- boss.cs
+++ boss.cs
@@ -7,8 +7,13 @@ public class boss : MonoBehaviour
 	Transform player;
     Rigidbody2D rb;
     public float speed = 5.5f;
+   
+    [Header("Patrol points")]
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
 
-	
+    [Header("Enemy")]
+    [SerializeField] private Transform enemy;
 
     void Start()
     {
@@ -20,8 +25,13 @@ public class boss : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Hero").transform;
 
-        Vector2 target = new Vector2(player.position.x, rb.position.y);
-        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
-        rb.MovePosition(newPos);
+        if (rb.position.x != leftEdge.position.x || rb.position.x != rightEdge.position.x)
+        {
+            Vector2 target = new Vector2(player.position.x, rb.position.y);
+            Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+            rb.MovePosition(newPos);
+        }
+        else { }
+
     }
 }
