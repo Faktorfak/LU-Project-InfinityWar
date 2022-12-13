2a14f85612888e276eeee3e56e670e6d238a9176
Faktorfak <101659073+Faktorfak@users.noreply.github.com>
2022-12-13 20:07:33 +0200

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -19,4 +19,22 @@ public class Menu : MonoBehaviour
     
     }
 
+    public void Level1() 
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+
+    }
+
 }
