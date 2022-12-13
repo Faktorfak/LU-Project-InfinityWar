0dcbea6e644b2d5419f6ac7ee7a8e513a4d34478
Faktorfak <101659073+Faktorfak@users.noreply.github.com>
2022-12-13 20:07:09 +0200

    Update BossHealth.cs

--- BossHealth.cs
+++ BossHealth.cs
@@ -7,20 +7,24 @@ public class BossHealth : MonoBehaviour
     public int health;
     int currentHealth;
     private Animator anim;
-    public bool bossIsAlive = true;
+    public static bool bossIsAlive = true;
     public static bool bossIsEnraged = false;
+    public BossHP bHP;
     private void Start()
     {
         currentHealth = health;
         anim = GetComponent<Animator>();
+        bHP.MaxHP(health);
+
     }
 
     public void TakeDamageB(int damage)
     {
         currentHealth -= damage;
-        Debug.Log(currentHealth);
+       
+        bHP.SetHelth(currentHealth);
 
-        if (currentHealth < 100)
+        if (currentHealth < 200)
         {
             bossIsEnraged = true;
         }
